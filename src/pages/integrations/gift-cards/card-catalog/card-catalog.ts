bdacfcc57a751ebf21306646bed19d9f28f205d1
Marcus L. Ludden <92890941+Noirsunset@users.noreply.github.com>
2022-04-25 03:24:42 +0700

    Initialize v5.6

---
+++ src/pages/integrations/gift-cards/card-catalog/card-catalog.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { BuyCardPage } from '../buy-card/buy-card';
+
+import { ActionSheetProvider } from '../../../../providers';
+import { GiftCardProvider } from '../../../../providers/gift-card/gift-card';
+import {
+  CardConfig,
+  CardName
+} from '../../../../providers/gift-card/gift-card.types';
+import { WideHeaderPage } from '../../../templates/wide-header-page/wide-header-page';
+
+@Component({
+  selector: 'card-catalog-page',
+  templateUrl: 'card-catalog.html'
+})
+export class CardCatalogPage implements OnInit {
+  public allCards: CardConfig[];
+  public featuredCards: CardConfig[];
+  public moreCards: CardConfig[];
+  public searchQuery: string = '';
+
+  @ViewChild(WideHeaderPage)
+  wideHeaderPage: WideHeaderPage;
+
+  constructor(
+    private actionSheetProvider: ActionSheetProvider,
+    private giftCardProvider: GiftCardProvider,
+    private navCtrl: NavController
+  ) {}
+
+  async ngOnInit() {
+    this.allCards = await this.giftCardProvider.getAvailableCards().catch(_ => {
+      this.showError();
+      return [] as CardConfig[];
+    });
+    this.updateCardList();
+  }
+
+  onSearch(query: string) {
+    this.searchQuery = query;
+    this.updateCardList();
+  }
+
+  updateCardList() {
+    const matchingCards = this.allCards.filter(c =>
+      isCardInSearchResults(c, this.searchQuery)
+    );
+    this.featuredCards = matchingCards.filter(c => isCardFeatured(c));
+    this.moreCards = matchingCards.filter(c => !isCardFeatured(c));
+  }
+
+  buyCard(cardConfig: CardConfig) {
+    this.navCtrl.push(BuyCardPage, { cardConfig });
+  }
+
+  private showError() {
+    const errorInfoSheet = this.actionSheetProvider.createInfoSheet(
+      'gift-cards-unavailable'
+    );
+    errorInfoSheet.present();
+    errorInfoSheet.onDidDismiss(() => this.navCtrl.pop());
+  }
+}
+
+export function isCardFeatured(c: CardConfig) {
+  const featuredCardNames = [
+    CardName.amazon,
+    CardName.amazonJapan,
+    CardName.delta,
+    CardName.googlePlay,
+    CardName.hotelsCom,
+    CardName.mercadoLibre,
+    CardName.uber,
+    CardName.uberEats
+  ];
+  return featuredCardNames.indexOf(c.name) !== -1;
+}
+
+export function isCardInSearchResults(c: CardConfig, search: string) {
+  const cardName = c.name.toLowerCase();
+  const query = search.toLowerCase();
+  const matchableText = [cardName, stripPunctuation(cardName)];
+  return matchableText.some(text => text.indexOf(query) > -1);
+}
+
+export function stripPunctuation(text: string) {
+  return text.replace(/[^\w\s]|_/g, '');
+}
