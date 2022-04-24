83355f70ab570eedee30409a14e101e5ca329b84
Marcus L. Ludden <92890941+Noirsunset@users.noreply.github.com>
2022-04-25 03:26:24 +0700

    Initialize revert-10926-bug/link-bp-card-01

---
+++ src/models/user/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  data?: {
+    givenName?: string;
+    familyName?: string;
+    email?: string;
+    eid?: string;
+    experiments?: { naDebit?: boolean };
+  };
+  error?: string;
+}
